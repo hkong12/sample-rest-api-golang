0f9c3842782f4cd760b734174acfd82fc016aa13
TiaTiaTia <kkonghc@gmail.com>
2021-03-17 00:49:19 +0800

    add sample output

--- jenkins_build.sh
+++ jenkins_build.sh
@@ -4,6 +4,7 @@ IMAGE_REPO_NAME='xueerk/sample-api'
 IMAGE_TAG=${BUILD_NUMBER}
 CLUSTER_NAME='sample-api-cluster'
 SERVICE_NAME='sample'
+TASK_FAMILY='sample-api'
 
 # Build docker image
 echo "Build started on `date`"
@@ -14,9 +15,9 @@ docker push $IMAGE_REPO_NAME:$IMAGE_TAG
 # Create new task definition for the build
 echo "Create new task definition"
 sed -e "s;%BUILD_NUMBER%;${BUILD_NUMBER};g" task_definition.json > task_definition_${BUILD_NUMBER}.json
-aws ecs register-task-definition --family sample-api --cli-input-json file://task_definition_${BUILD_NUMBER}.json
+aws ecs register-task-definition --cli-input-json file://task_definition_${BUILD_NUMBER}.json
 
 # Update the service with the new task definition and desired count
 echo "Update ECS service"
-TASK_REVISION=`aws ecs describe-task-definition --task-definition sample-api | egrep "revision" | tr "/" " " | awk '{print $2}' | sed 's/"$//'`
+TASK_REVISION=`aws ecs describe-task-definition --task-definition ${TASK_FAMILY} | egrep "revision" | tr "/" " " | awk '{print $2}' | sed 's/"$//'`
 aws ecs update-service --cluster ${CLUSTER_NAME} --service ${SERVICE_NAME} --task-definition ${TASK_FAMILY}:${TASK_REVISION} --desired-count 1
