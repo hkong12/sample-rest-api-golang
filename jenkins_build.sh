b0844432813a822db74d98b948cc5a623df3f7b8
TiaTiaTia <kkonghc@gmail.com>
2021-03-17 00:39:15 +0800

    update task_definition file

--- jenkins_build.sh
+++ jenkins_build.sh
@@ -19,9 +19,4 @@ aws ecs register-task-definition --family sample-api --cli-input-json file://tas
 # Update the service with the new task definition and desired count
 echo "Update ECS service"
 TASK_REVISION=`aws ecs describe-task-definition --task-definition sample-api | egrep "revision" | tr "/" " " | awk '{print $2}' | sed 's/"$//'`
-DESIRED_COUNT=`aws ecs describe-services --cluster ${CLUSTER_NAME} --services ${SERVICE_NAME} | egrep "desiredCount" | tr "/" " " | awk '{print $2}' | sed 's/,$//'`
-if [ ${DESIRED_COUNT} = "0" ]; then
-    DESIRED_COUNT="1"
-fi
-
-aws ecs update-service --cluster ${CLUSTER_NAME} --service ${SERVICE_NAME} --task-definition ${TASK_FAMILY}:${TASK_REVISION} --desired-count ${DESIRED_COUNT}
+aws ecs update-service --cluster ${CLUSTER_NAME} --service ${SERVICE_NAME} --task-definition ${TASK_FAMILY}:${TASK_REVISION} --desired-count 1
